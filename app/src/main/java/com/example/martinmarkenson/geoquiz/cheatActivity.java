412659f67526eff9b506138dee07a12244d46ce4
nartam11 <martyamark@gmail.com>
2015-12-05 22:25:10 -0600

    Added animation for Cheat button to disappear

--- app/src/main/java/com/example/martinmarkenson/geoquiz/cheatActivity.java
+++ app/src/main/java/com/example/martinmarkenson/geoquiz/cheatActivity.java
@@ -1,13 +1,17 @@
 package com.example.martinmarkenson.geoquiz;
 
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.content.Intent;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
+import android.animation.Animator;
 
 public class cheatActivity extends AppCompatActivity {
     private static final String EXTRA_ANSWER_IS_TRUE = "com.example.martinmarkenson.geoquiz.answeristrue";
@@ -26,7 +30,7 @@ public class cheatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
-        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE,false);
+        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
         mAnswerText = (TextView) findViewById(R.id.answerText);
         mShowAnswer = (Button) findViewById(R.id.show_answer_button);
 
@@ -39,6 +43,25 @@ public class cheatActivity extends AppCompatActivity {
                     mAnswerText.setText(R.string.false_button);
                 }
                 setAnswerShownResult(true);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    int cx = mShowAnswer.getWidth() / 2;
+                    int cy = mShowAnswer.getHeight() / 2;
+                    float radius = mShowAnswer.getWidth();
+                    Animator anim = ViewAnimationUtils.createCircularReveal(mShowAnswer, cx, cy, radius, 0);
+                    anim.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            mAnswerText.setVisibility(View.VISIBLE);
+                            mShowAnswer.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    anim.start();
+                } else {
+                    mAnswerText.setVisibility(View.VISIBLE);
+                    mShowAnswer.setVisibility(View.INVISIBLE);
+                }
             }
 
         });
